6fb9bc065091786fd180ff7eadfac25005714511
Deiver Vasquez <estudiandovazmore@gmail.com>
2023-10-15 21:57:41 -0500

    change received data in body

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ app = Flask(__name__)
 
 class GenerateQRView(views.MethodView):
     def post(self):
-        data = request.json or {}
+        data = request.data or {}
         if os.path.exists('test.jpeg'):
             os.remove('test.jpeg')
 
         if not data:
             return CustomResponse({"message": 'Data not found'}, status=404)
 
-        qr_generator = QrGenerator().encrypt_data(data)
+        qr_generator = QrGenerator().encrypt_data(json.loads(data))
         image = qr_generator.generate_image()
 
         image.save('test.jpeg', format="JPEG")
